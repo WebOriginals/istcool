fcc703871e047563a2763c5f7b4f99d9f84c4140
WebOriginals <web-originals@yandex.ru>
2021-04-06 18:01:59 +0300

    десктоп карточка товара

--- #src/js/sliders/pageCardGood-sl.js
+++ #src/js/sliders/pageCardGood-sl.js
@@ -2,40 +2,33 @@ if( $( '.slider-little-container' ).length ) {
     var sliderLlittle = new Swiper('.slider-little-container', {
         slidesPerView: 1,
         spaceBetween: 15,
-        loop:true,
         lazy: true,
         slideToClickedSlide:true,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
         breakpoints: {
-            767: {
+            300: {
                 direction: 'horizontal',
                 slidesPerView: 3,
             },
 
-            1280: {
+            1023: {
                 direction: 'vertical',
                 slidesPerView: 3,
+                spaceBetween: 25,
             },
         }
     });
 
-    if(window.screen.width>=767) {
-        var sliderLarge = new Swiper('.slider-large-container', {
-            lazy: true,
-            thumbs: {
-                swiper: sliderLlittle
-            }
-        });
-    } else {
-        var sliderLarge = new Swiper('.slider-large-container', {
-            lazy: true,
-            pagination: {
-                el: '.slider-large-pagination',
-                clickable: true,
-            },
-        });
-    }
+
+    var sliderLarge = new Swiper('.slider-large-container', {
+        lazy: true,
+        spaceBetween: 15,
+        thumbs: {
+            swiper: sliderLlittle
+        }
+    });
+
 };
 
 
