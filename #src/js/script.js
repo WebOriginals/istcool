fec40d74f37f99403b83c092664700e2952434eb
WebOriginals <web-originals@yandex.ru>
2021-04-21 14:39:36 +0300

    убрал карту и селект

--- #src/js/script.js
+++ #src/js/script.js
@@ -8,10 +8,8 @@ $( document ).ready(function() {
     //component
     @@include('../component/Modals/js/modal.js')
     @@include('../component/Tabs/js/tabs.js')
-    @@include('../component/InputList/js/InputList.js')
     @@include('../component/dropDownLlist/js/dropDownList.js')
     @@include('../component/Quantity/js/quantity.js')
-    @@include('../component/map/js/map.js')
     // end component
 
 
