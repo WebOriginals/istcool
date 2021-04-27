bd91cc84a2fb1c0317f3927dc1225caf39ac548b
WebOriginals <web-originals@yandex.ru>
2021-04-27 13:04:57 +0300

    переделал главный слайдер

--- #src/js/sliders/pageMain-firstScreen-sl.js
+++ #src/js/sliders/pageMain-firstScreen-sl.js
@@ -1,12 +1,18 @@
 if ($('.firstScreen-container').length) {
     var swiper = new Swiper('.firstScreen-container', {
         lazy: true,
+        autoplay: {
+            delay: 5000,
+        },
         autoHeight: true,
         spaceBetween:15,
         pagination: {
             el: '.firstScreen-pagination',
             clickable: true,
         },
-
+        navigation: {
+            nextEl: '.first-screen-button-next',
+            prevEl: '.first-screen-button-prev',
+        },
     });
 };
--- dist/js/script.js
+++ dist/js/script.js
@@ -253,13 +253,19 @@ document.addEventListener('keydown', function (e) {
     if ($('.firstScreen-container').length) {
     var swiper = new Swiper('.firstScreen-container', {
         lazy: true,
+        autoplay: {
+            delay: 5000,
+        },
         autoHeight: true,
         spaceBetween:15,
         pagination: {
             el: '.firstScreen-pagination',
             clickable: true,
         },
-
+        navigation: {
+            nextEl: '.first-screen-button-next',
+            prevEl: '.first-screen-button-prev',
+        },
     });
 };
     if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
