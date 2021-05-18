9db6ec13c09a33ab78f83991435268272eced527
WebOriginals <web-originals@yandex.ru>
2021-05-18 12:45:59 +0300

    сделал хелпер для доп опций

--- dist/js/script.js
+++ dist/js/script.js
@@ -571,4 +571,23 @@ $(".sher-close").click(function () {
 
 
 
+    $(".helper-ic").click(function () {
+
+    var elem = this;
+    var body = $(elem).closest('.checkbox');
+    var text = $(body).find('.helper-body');
+
+    $(text).addClass("active");
+});
+
+$(".helper-close").click(function () {
+
+    var elem = this;
+    var body = $(elem).closest('.checkbox');
+    var text = $(body).find('.helper-body');
+
+    $(text).removeClass("active");
+});
+
+
 })
